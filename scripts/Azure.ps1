0baacef7b119b8737953c76bcfc2b3c91adc9554
Iñaki Elcoro <iceclow@gmail.com>
2019-01-08 07:51:22 +0100

    Add Service Bus and Functions tools

--- scripts/Azure.ps1
+++ scripts/Azure.ps1
@@ -1,3 +1,5 @@
 choco install -y azure-cli
 Install-Module -Force Az
 choco install -y microsoftazurestorageexplorer
+choco install -y servicebusexplorer
+choco install -y azure-functions-core-tools

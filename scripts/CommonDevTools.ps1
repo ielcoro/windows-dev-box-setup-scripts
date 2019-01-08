e0ad6345911fe058a7f08272e904cd2b89ba7abe
Iñaki Elcoro <iceclow@gmail.com>
2019-01-08 07:31:59 +0100

    Adapta Common Dev Tools

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,9 @@
 
 # tools we expect devs across many scenarios will want
 choco install -y vscode
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y python
+choco install sublimetext3
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /NoAutoCrlf'"
+choco install pwsh
+#choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
